4e0a88160956dc7bd688da865e2b8563b9abb612
huangfusuper <huangfusuper@163.com>
2020-10-12 11:06:24 +0800

    处理数据库链接信息

--- example-payment/src/main/java/com/example/mapper/PaymentMapper.java
+++ example-payment/src/main/java/com/example/mapper/PaymentMapper.java
@@ -2,6 +2,7 @@ package com.example.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.entity.Payment;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * *********************************************************************
@@ -11,5 +12,6 @@ import com.example.entity.Payment;
  * @author huangfu
  * @date 2020/10/11 21:30
  */
+@Mapper
 public interface PaymentMapper extends BaseMapper<Payment> {
 }
